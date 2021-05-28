2a8842a4bf17077652a4ec09047a5d0a97c9e15c
Jake Wood <jakealistairwood@gmail.com>
2021-05-28 13:01:25 +0100

    filters modal functionality in situ

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,30 +1,72 @@
 import { useContext } from 'react';
 import FiltersContext from '../context/filtersContext'
+import ApiContext from '../context/apiContext'
 import { XIcon } from '@heroicons/react/solid'
 
 const Filters = () => {
 
     const { filtersOpen, toggleFilters } = useContext(FiltersContext);
+    const { minABV, setMinABV, maxABV, setMaxABV, fetchBeers } = useContext(ApiContext);
 
     const toggleFiltersModal = 
     filtersOpen 
     ? 'fixed flex justify-center items-center top-0 left-0 right-0 h-screen w-screen bg-black bg-opacity-60 z-200' 
     : 'hidden';
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchBeers();
+    }
+
     return (
         <div className={toggleFiltersModal}>
-            <form className="flex flex-col z-999 w-1/3 bg-white p-6 rounded-md">
-                <div className="flex justify-between items-center">
+            <form className="flex flex-col z-999 w-1/3 bg-white p-6 rounded-md" onSubmit={handleSubmit}>
+                <div className="flex justify-between items-center mb-6">
                     <h3 className="text-2xl font-bold">Filter Options</h3>
                     <XIcon className="h-6 text-gray-500 cursor-pointer" onClick={toggleFilters} />
                 </div>
-                <div>
-                    <label htmlFor="minAbv"></label>
-                    <input 
-                        type="range" 
-                        name="" 
-                        id="minAbv" 
-                    />
+                <div className="flex flex-col">
+                    <div className="flex flex-col">
+                        <label htmlFor="minABV">Min ABV</label>
+                        <div className="flex mb-4">
+                            <label>{minABV}%</label>
+                            <input
+                                className="w-full ml-6" 
+                                type="range" 
+                                name="minABV" 
+                                id="minABV"
+                                min="0"
+                                max={maxABV}
+                                step="0.2"
+                                value={minABV}
+                                onInput={(e) => {
+                                    setMinABV(e.target.value);
+                                    fetchBeers();
+                                }} 
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="maxABV">Max ABV</label>
+                        <div className="flex mb-4">
+                            <label>{maxABV}%</label>
+                            <input
+                                className="w-full ml-6" 
+                                type="range" 
+                                name="maxABV" 
+                                id="maxABV"
+                                min={minABV}
+                                max="20"
+                                step="0.2"
+                                value={maxABV}
+                                onInput={(e) => {
+                                    setMaxABV(e.target.value);
+                                    fetchBeers();
+                                }} 
+                            />
+                        </div>
+                    </div>
+                    <button className="bg-gray-800 rounded-full w-max py-2 px-6 text-white font-bold" onClick={toggleFilters}>Submit</button>
                 </div>
             </form>    
         </div>
--- src/context/apiContext.js
+++ src/context/apiContext.js
@@ -7,13 +7,15 @@ export const ApiProvider = ({ children }) => {
 
     const [ beers, setBeers ] = useState([]);
     const [ beerName, setBeerName ] = useState();
+    const [ minABV, setMinABV ] = useState(0);
+    const [ maxABV, setMaxABV ] = useState(20);
 
-    const searchByName = beerName ? `beer_name=${beerName}` : '';
-
-    console.log(beers);
+    const searchByName = beerName ? `&beer_name=${beerName}` : '';
+    const searchByMinABV = `&abv_gt=${minABV}`;
+    const searchByMaxABV = `&abv_lt=${maxABV}`;
 
     const fetchBeers = async() => {
-        const res = await axios.get(`https://api.punkapi.com/v2/beers?${searchByName}`);
+        const res = await axios.get(`https://api.punkapi.com/v2/beers?per_page=48${searchByName}${searchByMinABV}`);
         setBeers(res.data);
     }
 
@@ -22,7 +24,16 @@ export const ApiProvider = ({ children }) => {
     }, [])
 
     return (
-        <ApiContext.Provider value={{ beers, fetchBeers, setBeerName }}>
+        <ApiContext.Provider value={
+            { beers, 
+              fetchBeers, 
+              setBeerName,
+              minABV, 
+              setMinABV,
+              maxABV,
+              setMaxABV 
+            }
+        }>
             {children}
         </ApiContext.Provider>
     )
